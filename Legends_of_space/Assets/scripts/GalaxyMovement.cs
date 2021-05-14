a42d3fbde054060a46c39c019b3877d854009dd5
TossACoinToYourTeacher <59828461+TossACoinToYourTeacher@users.noreply.github.com>
2021-05-14 16:20:02 +0200

    Second advance, galaxy movement and initial try to rotate the red spaceship

---
+++ Legends_of_space/Assets/scripts/GalaxyMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GalaxyMovement : MonoBehaviour
+{
+    public float random;
+
+    public float x;
+    public float y;
+    public float z;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         x = transform.position.x;
+         y = transform.position.y;
+         z = transform.position.z;
+
+        random = Random.Range(0.0f, 1.0f);
+        if (random > 0.4)
+        {
+            transform.Translate(speed, 0.0f, speed/2, Space.World);
+        }
+        else
+        {
+            transform.Translate(speed/2, 0.0f, speed, Space.World);
+        }
+
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+
+        if ( x > 100.0f )
+        {
+            transform.Translate(-99.0f, 0.0f, -z/3);
+        }
+        else if ( x < 0.0f )
+        {
+            //transform.Translate(-(x-1), 0.0f, -z/3);
+
+        }
+
+        if (z > 100.0f)
+        {
+            transform.Translate(-x/3, 0.0f, -99.0f);
+        }
+        else if (z < 0.0f)
+        {
+            //transform.Translate(-x/3, 0.0f, -(z-1));
+
+        }
+
+        transform.Rotate(0, 0.5f, 0);
+    }
+}
---
+++ Legends_of_space/Assets/scripts/RedSpaceship.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedSpaceship : MonoBehaviour
+{
+    public FiducialController fc;
+    public float angle;
+
+    private Vector3 last_position;
+    private Vector3 actual_position;
+    private Vector3 change_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        last_position = fc.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actual_position = fc.transform.position;
+        change_position = actual_position - last_position;
+
+        //Debug.Log(change_position);
+        if (change_position.x > 0)
+        {
+            fc.transform.Rotate(0, angle * Time.deltaTime, 0);
+        }
+        if (change_position.x < 0)
+        {
+            fc.transform.Rotate(0, -angle * Time.deltaTime, 0);
+        }
+
+        float y_rotation = fc.transform.eulerAngles.y;
+
+        if (change_position.z > 0 && y_rotation > 0)
+        {
+            fc.transform.Rotate(0, -( y_rotation - (0.15f * y_rotation) )  * Time.deltaTime, 0);
+        }
+        if (change_position.z > 0 && y_rotation < 0)
+        {
+            fc.transform.Rotate(0, (y_rotation - (0.15f * y_rotation)) * Time.deltaTime, 0);
+        }
+
+        if (change_position.z < 0 && y_rotation > 0)
+        {
+            fc.transform.Rotate(0, (y_rotation - (0.15f * y_rotation)) * Time.deltaTime, 0);
+        }
+        if (change_position.z < 0 && y_rotation < 0)
+        {
+            fc.transform.Rotate(0, -(y_rotation - (0.15f * y_rotation)) * Time.deltaTime, 0);
+        }
+
+        last_position = actual_position;
+    }
+}
