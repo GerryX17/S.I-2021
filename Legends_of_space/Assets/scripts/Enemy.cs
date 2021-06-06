622fa098ad555dd8321ba54e895d9207f3832b26
GerryX17 <gerard.opazo01@estudiant.upf.edu>
2021-06-07 01:06:14 +0200

    update

---
+++ Legends_of_space/Assets/scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float runSpeed;
+    public float gotShotDestroyDelay;
+    private bool hitByShot;
+
+    public float dropDestroyDelay; // 1
+    private Collider myCollider; // 2
+    private Rigidbody myRigidbody;
+    private EnemySpawner enemySpawner;
+    public float heartOffset; // 1
+    public GameObject heartPrefab; // 2
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+
+    }
+
+    private void Drop()
+    {
+        enemySpawner.RemoveEnemyFromList(gameObject, hitByShot);
+        myRigidbody.isKinematic = false; // 1
+        myCollider.isTrigger = false; // 2
+        Destroy(gameObject, dropDestroyDelay); // 3
+        SoundManager.Instance.PlayShipDroppedClip();
+
+    }
+
+
+    private void HitByHay()
+    {
+        hitByShot = true; // 1
+        enemySpawner.RemoveEnemyFromList(gameObject, hitByShot);
+        runSpeed = 0; // 2
+        Destroy(gameObject, gotShotDestroyDelay); // 3
+
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+        //TweenScale tweenScale = gameObject.AddComponent<TweenScale>(); ; // 1
+        //tweenScale.targetScale = 0; // 2
+        //tweenScale.timeToReachTarget = gotShotDestroyDelay; // 3
+        SoundManager.Instance.PlayShipHitClip();
+
+    }
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag("Laser") && !hitByShot) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+
+    }
+
+    public void SetSpawner(EnemySpawner spawner)
+    {
+        enemySpawner = spawner;
+    }
+}
---
+++ Legends_of_space/Assets/scripts/NebulaTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NebulaTrigger : MonoBehaviour
+{
+    public string tagPlayer1;
+    public string tagPlayer2;
+    public GameObject enemyPrefab; 
+    public List<Transform> enemySpawnPositions = new List<Transform>(); 
+    public float timeBetweenSpawns;
+    private List<GameObject> enemyList = new List<GameObject>(); 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag(tagPlayer1) || other.CompareTag(tagPlayer2))
+        {
+            Vector3 randomPosition = enemySpawnPositions[Random.Range(0, enemySpawnPositions.Count)].position; 
+            GameObject enemy = Instantiate(enemyPrefab, randomPosition, enemyPrefab.transform.rotation); 
+            enemyList.Add(enemy); 
+
+        }
+    }
+}
