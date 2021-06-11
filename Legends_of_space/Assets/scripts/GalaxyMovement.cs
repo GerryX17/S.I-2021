1e6df741700b2dd30280f2323480afe026cdf7d6
GerryX17 <gerard.opazo01@estudiant.upf.edu>
2021-06-11 18:47:38 +0200

    Update

--- Legends_of_space/Assets/scripts/GalaxyMovement.cs
+++ Legends_of_space/Assets/scripts/GalaxyMovement.cs
@@ -15,6 +15,8 @@ public class GalaxyMovement : MonoBehaviour
 
     public float speed;
 
+    private Vector3 lastVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +27,18 @@ public class GalaxyMovement : MonoBehaviour
     void Update()
     {
         t.Rotate(0, 0.5f, 0);
+        transform.Translate(speed, 0.0f, speed / 2, Space.World);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CompareTag("Wall"))
+        {
+            speed = lastVelocity.magnitude;
+            //var direction = Vector3.Reflect(lastVelocity.normalized);
+            // transform.velocity = direction * Math.(speed, 0f);
+        }
     }
 }
